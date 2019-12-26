f87dd2434abc043ece867a1f246ebe18420b9c5a
t17cs007 <t17cs007@yamanashi.ac.jp>
2019-12-27 02:13:33 +0900

    [modify] メールアドレスであるかの判定を追加

--- AdmissionApplication/views.py
+++ AdmissionApplication/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.views.generic.edit import CreateView
 from .models import User
 from django.views.generic.base import TemplateView
+import re
 
 # Create your views here.
 def MenuView(request):
@@ -18,18 +19,17 @@ class UserAddView(CreateView):
     def form_valid(self, form):
         ctx = {'form': form}
         if self.request.POST.get('next', '') == 'confirm':
-            return render(self.request, 'AdmissionApplication/confirm.html', ctx)
+            if ("-" in self.request.POST.get("phone_number")) == False:
+                return render(self.request, 'AdmissionApplication/warning_phone.html', ctx)
+            elif (re.match('[A-Za-z0-9\._+]+@[A-Za-z]+\.[A-Za-z]', self.request.POST.get("mail_address")) == None) :
+                return render(self.request, 'AdmissionApplication/warning_mail.html', ctx)      
+            elif self.request.POST.get("entrance_schedule") > self.request.POST.get("exit_schedule"):
+                return render(self.request, 'AdmissionApplication/warning_schedule.html', ctx)
+            else:
+                return render(self.request, 'AdmissionApplication/confirm.html', ctx)
+            
         if self.request.POST.get('next', '') == 'back':
-            return render(self.request, 'AdmissionApplication/admission.html', ctx)
-        
-        if ("-" in self.request.POST.get("phone_number")) == False:
-            return HttpResponseRedirect('../../admin')      
-        
-        if ("@" in self.request.POST.get("mail_address")) == False:
-            return HttpResponseRedirect('../../admin')
-       
-        if self.request.POST.get("entrance_schedule") > self.request.POST.get("exit_schedule"):
-            return  HttpResponseRedirect('../../admin')
-        
+            return render(self.request, 'AdmissionApplication/admission.html', ctx)      
+         
         if self.request.POST.get('next', '') == 'create':
             return super().form_valid(form)
